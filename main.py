631342e7464e0562b517d693d64c3d2c2257795c
Jesper Armouti-Hansen <jesper@armoutihansen.xyz>
2026-02-23 16:54:59 +0100

    Adding user prompt capability and --verbose option

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
@@ -9,10 +11,23 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
+    if response.usage_metadata is None:
+        raise RuntimeError("Response is missing usage metadata")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
